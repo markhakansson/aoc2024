ce55f303d6d66e0d86b58e96d09eafba0e07ff46
markhakansson <mark.hakansson@live.se>
2024-12-07 15:45:52 +0100

    Format solution for day 1

--- solution/day1.exs
+++ solution/day1.exs
@@ -3,19 +3,20 @@ defmodule HistorianHysteria do
   Usage `cat <input> | elixir day1.exs`.
   """
 
-	def run do
-    {first,  second} = read_stdin()
-  	  |> split
-  	
-  	solve_1(first, second)
-  	  |> IO.puts
+  def run do
+    {first, second} =
+      read_stdin()
+      |> split
 
-  	solve_2(first, second)
-  	  |> IO.puts
+    solve_1(first, second)
+    |> IO.puts()
+
+    solve_2(first, second)
+    |> IO.puts()
   end
 
-  def read_stdin do 
-  	IO.read(:stdio, :eof)
+  def read_stdin do
+    IO.read(:stdio, :eof)
   end
 
   @doc """
@@ -23,9 +24,9 @@ defmodule HistorianHysteria do
   """
   def split(input) do
     input
-    	|> String.split([" ", "\n"], trim: true)
-      |> Enum.map(&String.to_integer/1)
-  	  |> Enum.reduce({[], []}, fn x, {curr, next} -> { next, [x | curr]} end)
+    |> String.split([" ", "\n"], trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce({[], []}, fn x, {curr, next} -> {next, [x | curr]} end)
   end
 
   @doc """
@@ -33,25 +34,27 @@ defmodule HistorianHysteria do
   """
   def solve_1(l1, l2) do
     [Enum.sort(l1), Enum.sort(l2)]
-      |> Enum.zip()
-      |> Enum.map(fn {a, b} -> Kernel.abs(a - b) end)
-      |> Enum.sum
+    |> Enum.zip()
+    |> Enum.map(fn {a, b} -> Kernel.abs(a - b) end)
+    |> Enum.sum()
   end
 
   @doc """
   Solution for the second puzzle.
   """
-  def solve_2(l1, l2) do 
+  def solve_2(l1, l2) do
     frequencies = Enum.frequencies(l2)
+
     l1
-      |> Enum.map(fn x -> 
-           frequency = Map.get(frequencies, x)
-           case frequency do
-             nil -> 0
-             _ -> x * frequency
-           end
-         end)
-      |> Enum.sum
+    |> Enum.map(fn x ->
+      frequency = Map.get(frequencies, x)
+
+      case frequency do
+        nil -> 0
+        _ -> x * frequency
+      end
+    end)
+    |> Enum.sum()
   end
 end
 
