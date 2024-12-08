3f9d87046f926159ff1b3c8305235621ad0c72fc
markhakansson <mark.hakansson@live.se>
2024-12-08 13:54:36 +0100

    Format solution for day 2

--- solution/day2.exs
+++ solution/day2.exs
@@ -3,13 +3,14 @@ defmodule RedNosedReports do
   Usage `cat <input> | elixir day2.exs`.
   """
 
-	def run do
-  	input = read_stdin()
-  	|> split
+  def run do
+    input =
+      read_stdin()
+      |> split
 
-  	input
-		|> solve1
-		|> IO.puts
+    input
+    |> solve1
+    |> IO.puts()
   end
 
   def read_stdin do
@@ -23,60 +24,61 @@ defmodule RedNosedReports do
   end
 
   def split_line(input) do
-		input
-		|> String.split([" "], trim: true)
+    input
+    |> String.split([" "], trim: true)
     |> Enum.map(&String.to_integer/1)
   end
 
   def solve1(input) do
-  	input
-  	|> Enum.map(&RedNosedReports.solve_report/1)
-  	|> Enum.count(fn x -> x == true end)
+    input
+    |> Enum.map(&RedNosedReports.solve_report/1)
+    |> Enum.count(fn x -> x == true end)
   end
 
   def solve_report(report) do
     d = diff(report)
 
-  	# check level change is ok
-		level_change_ok = is_level_change_ok(d)
+    # check level change is ok
+    level_change_ok = is_level_change_ok(d)
 
-  	# check direction does not change
-  	direction_ok = is_direction_ok(d)
+    # check direction does not change
+    direction_ok = is_direction_ok(d)
 
-  	level_change_ok && direction_ok
-	 end
+    level_change_ok && direction_ok
+  end
 
-	def diff(report) do
-		report
-		|> Enum.chunk_every(2, 1, :discard)
-		|> Enum.map(fn [x, y] -> y - x end)
+  def diff(report) do
+    report
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [x, y] -> y - x end)
   end
 
   # Check if direction never changes
-	def is_direction_ok(diff) do
-		sum = diff
-		|> Enum.map(fn x -> 
-		  case x do
-        z when z < 0 -> -1
-        z when z > 0 -> 1
-        _ -> 0
-		  end
-		end)
-		|> Enum.sum
+  def is_direction_ok(diff) do
+    sum =
+      diff
+      |> Enum.map(fn x ->
+        case x do
+          z when z < 0 -> -1
+          z when z > 0 -> 1
+          _ -> 0
+        end
+      end)
+      |> Enum.sum()
 
-		Kernel.abs(sum) == length(diff)
-	end
+    Kernel.abs(sum) == length(diff)
+  end
 
-	def is_level_change_ok(diff) do
-		diff
-		|> Enum.map(&Kernel.abs/1)
-  	|> Enum.map(fn x -> 
-  		case x do
-				z when z in 1..3 -> true
-				_ -> false
-  		end
-  	end)
-  	|> Enum.reduce(fn x, acc -> x && acc end)
+  def is_level_change_ok(diff) do
+    diff
+    |> Enum.map(&Kernel.abs/1)
+    |> Enum.map(fn x ->
+      case x do
+        z when z in 1..3 -> true
+        _ -> false
+      end
+    end)
+    |> Enum.reduce(fn x, acc -> x && acc end)
   end
 end
 
